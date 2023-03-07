edd0760aaa9132ecb877e734f0a70a82dd883ac2
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-03-07 22:25:43 +0100

    tests

--- src/main/java/javadb/client/tests/SubscribeTest.java
+++ src/main/java/javadb/client/tests/SubscribeTest.java
@@ -4,16 +4,19 @@ import javadb.client.ClientSubscriber;
 import javadb.client.ClientTask;
 import tables.User;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SubscribeTest {
     public static void main(String[] args) throws InterruptedException {
         ClientSubscriber<User[]> usersSubscriber = new ClientSubscriber<>("koebstoffer.info", tables -> {
             return tables.users.stream().toList().toArray(new User[0]);
         }, users -> {
-            System.out.println("Found: " + users);
+            System.out.println("Updated users:");
+            for (User user : users) {
+                System.out.println(user);
+            }
         },error -> {
             System.out.println("Failed to receive users: " + error.getMessage());
         });
-
     }
 }
